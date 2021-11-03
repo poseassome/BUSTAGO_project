0d253f605cd316fc54b8c78135ba631bfa2c9816
jiyoung <higomn05@gmail.com>
2021-11-04 01:10:33 +0900

    First Commit

---
+++ script/index.js
@@ -0,0 +1,59 @@
+window.onload = function () {
+  // GNB
+  var gnb = document.querySelector(".gnb>ul");
+  gnb.onmouseover = function () {
+
+  }
+
+  // 메인 1단 메뉴
+  var menu = document.querySelectorAll(".main_cont1>ul>li>a");
+  var menu1 = menu[0];
+  var menu2 = menu[1];
+  var menu3 = menu[2];
+
+  menu1.onclick = function () {
+    if (menu1.classList.contains("tk_off") == true) {
+      menu1.classList.remove("tk_off");
+
+      var menuoff = document.querySelector(".tk_on");
+      menuoff.classList.remove("tk_on");
+      menuoff.classList.add("tk_off");
+
+      menu1.classList.add("tk_on");
+
+      menu1.nextElementSibling.style.display = "block";
+      menuoff.nextElementSibling.classList.add("form_off");
+    } else { };
+  };
+
+  menu2.onclick = function () {
+    if (menu2.classList.contains("tk_off") == true) {
+      menu2.classList.remove("tk_off");
+
+      var menuoff = document.querySelector(".tk_on");
+      menuoff.classList.remove("tk_on");
+      menuoff.classList.add("tk_off");
+
+      menu2.classList.add("tk_on");
+
+      menu2.nextElementSibling.classList.remove("form_off");
+      menuoff.nextElementSibling.classList.add("form_off");
+
+    } else { };
+  };
+
+  menu3.onclick = function () {
+    if (menu3.classList.contains("tk_off") == true) {
+      menu3.classList.remove("tk_off");
+
+      var menuoff = document.querySelector(".tk_on");
+      menuoff.classList.remove("tk_on");
+      menuoff.classList.add("tk_off");
+
+      menu3.classList.add("tk_on");
+
+      menu3.nextElementSibling.classList.remove("form_off");
+      menuoff.nextElementSibling.classList.add("form_off");
+    } else { };
+  };
+};
