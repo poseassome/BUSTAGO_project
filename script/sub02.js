5a81ef137d0431a93b537a4f6a42488f20fd364f
jiyoung <higomn05@gmail.com>
2021-11-16 03:38:43 +0900

    add jquery

--- script/sub02.js
+++ script/sub02.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  //// 버스회사 안내
   $(".type_area ul").slideUp(0);
   $(".type_area h4").mouseenter(function () {
     $(this).css({ cursor: "pointer" });
@@ -16,9 +17,10 @@ $(document).ready(function () {
     $(".type_area ul").fadeOut(0);
   });
 
+  //// 노선 안내
   // datepicker
   $("#leave_day").datepicker({
-    showOn: "button", buttonImage: "./images/icon_date.jpg", showOtherMonths: true
+    showOn: "both", buttonImage: "./images/icon_date.jpg", showOtherMonths: true
   });
   $.datepicker.setDefaults({
     dateFormat: 'yy-mm-dd', buttonText: "선택", yearSuffix: "년", monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'], monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'], dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'], dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'], showMonthAfterYear: true, prevText: '이전 달',
@@ -28,4 +30,49 @@ $(document).ready(function () {
     }, minDate: "D", maxDate: "+2M"
   });
   $("#leave_day").datepicker('setDate', 'today');
+
+  // Options
+  $(document).click(function (e) {
+    if (!$(e.target).is(".time_wrap, #leave_time, a")) {
+      var container = $(".time_list");
+      container.slideUp(0);
+    };
+  });
+  $(".ui-datepicker-trigger").click(function () {
+    $(".time_list").slideUp(200);
+  })
+  $(".time_wrap").click(function () {
+    $(this).next("ul").slideToggle(200);
+  });
+  $(".time_list a").hover(function () {
+    $(this).css({ color: "#272343", "font-weight": "bold" });
+  }, function () {
+    $(this).css({ color: "" });
+  });
+  $(".time_list a").click(function () {
+    var text = $(this).text();
+    $(this).parents("ul").prev("div").find("input").val(text);
+    $(this).closest("ul").slideUp(200);
+  });
+  $(".route_inform_search").click(function () {
+    if ($("#depart_spot").val() == "") {
+      alert("출발지를 입력해주세요.");
+      $("#depart_spot").focus();
+      return false;
+    } else if ($("#arrive_spot").val() == "") {
+      alert("도착지를 입력해주세요.");
+      $("#arrive_spot").focus();
+      return false;
+    } else {
+      $(".start").text($("#depart_spot").val());
+      $(".end").text($("#arrive_spot").val());
+    }
+  });
+
+  //// 터미널 안내
+  $(".terminal_search_wrap ul a").eq(0).addClass("on");
+  $(".terminal_search_wrap ul a").click(function () {
+    $(".terminal_search_wrap ul a").removeClass("on");
+    $(this).addClass("on");
+  });
 });
--- script/sub04.js
+++ script/sub04.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  // 자주묻는질문(FAQ)
+  //// 자주묻는질문(FAQ)
   $(".type_faq_wrap a").first().addClass("on");
   $(".type_faq_wrap a").click(function () {
     $(".type_faq_wrap a").removeClass("on");
@@ -9,5 +9,25 @@ $(document).ready(function () {
     $(".faq_questline").removeClass("on");
     $(this).addClass("on");
     $(this).after("<li class='faq_answerline'>" + "비회원으로도 동일하게 예매, 조회, 변경, 취소 가능합니다." + "</li>");
+  });
+
+  //// 일대일 문의하기
+  $(".opinion_type input").click(function () {
+    $(".opinion_type ul").fadeToggle(100);
+  });
+  $(".opinion_type ul a").hover(function () {
+    $(this).css({ color: "#272343", "font-weight": "bold" });
+  }, function () {
+    $(this).css({ color: "" });
+  });
+  $(".opinion_type ul a").click(function () {
+    $("#op_type").val($(this).text());
+    $(this).parents("ul").fadeOut(100);
+  });
+  $(document).click(function (e) {
+    if (!$(e.target).is("#op_type")) {
+      var container = $(".opinion_type ul");
+      container.slideUp(0);
+    };
   });
 });
